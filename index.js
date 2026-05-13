af4e2db1d0fccbbc39cb8d9510f3708fe496cac1
OfficiallyDragon <altofficiallydragon@gmail.com>
2026-05-13 13:51:41 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, EmbedBuilder, Embed } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates] });
 const targetMoveChannel = '985273253360726017';
 const solitaryConfinement = '1322320398603391059';
 const Alex = '1052255855518695554';
+const BotId = '1351674429935063070';
 //log login event
 client.once(Events.ClientReady, (readyClient) => {
 	console.log(`${readyClient.user.displayName} is ready!`);
@@ -28,8 +29,7 @@ client.on(Events.MessageCreate, async (message) => {
 	const quotesChannel = quotesChannelId = '986667448449241129';
 	if (message.channel.id === quotesChannel) {
 		try {
-			await message.react('⬆️');
-			await message.react('⬇️');
+			await message.react('⬆️','⬇️');
 		} catch (err) {
 			console.error('Failed to add reactions:', err);
 		}
@@ -42,9 +42,15 @@ client.on(Events.MessageCreate, async (message) => {
 	const clipChannel = clipChannelId = '1504088140061741066';
 	if (message.channel.id === clipChannel) {
 		try {
-			if (message.content.includes('https://medal.tv/')) {
-				const clip = message.content.toString();
-				message.reply("test");
+			if (message.content.includes('https://medal.tv/games/valorant')) {
+				message.reply(new EmbedBuilder()
+				.setImage('https://x.com/VALORANT/photo')
+				.setDescription()
+				.setFooter('Please vote for this clip by reacting with ⬆️ or ⬇️')
+			 	);
+				if (message.author.id === BotId) {
+					message.react('⬆️','⬇️');
+				}
 			}
 		} catch (err) {
 			console.error('Failed to resend clip:', err);
